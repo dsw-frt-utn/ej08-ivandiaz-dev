0082ad1a01d41d4e188f1fa5533404018336e543
Diaz Orellana Ivan Rodrigo <ivandiaz.arg.dev@gmail.com>
2026-05-04 20:23:43 -0300

    Resolucion Problema N4

---
+++ Dsw2026Ej8/Problema4.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    public static class Problema4
+    {
+        public static double CalcularPromedio(int? nota1, int? nota2, int? nota3)
+        {
+            int suma = 0;
+            int cantidad = 0;
+
+            void verificarNota(int? nota)
+            {
+                if(nota.HasValue && nota >= 0 && nota <= 10)
+                {
+                    suma += nota.Value;
+                    cantidad++;
+                }
+            }
+
+            verificarNota(nota1);
+            verificarNota(nota2);
+            verificarNota(nota3);
+
+            return cantidad == 0 ? 0 : (double)suma / cantidad;
+        }
+    }
+}
