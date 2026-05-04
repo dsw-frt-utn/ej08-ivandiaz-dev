015d3ccec3829b63f5948d1e816c8e8ec36fa289
Diaz Orellana Ivan Rodrigo <ivandiaz.arg.dev@gmail.com>
2026-05-04 20:09:20 -0300

    Resolucion Ejercicio N3

---
+++ Dsw2026Ej8/Problema3.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    public static class Problema3
+    {
+        public static string CompararCopias(int originalValue, Product product)
+        {
+            int copia = originalValue;
+            copia++;
+
+            Product productoCopia = product;
+            productoCopia.modificarDescripcion("Este es un producto modificado");
+
+            return $"{originalValue}-{copia}-{productoCopia.description}";
+        }
+    }
+}
---
+++ Dsw2026Ej8/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    public class Product
+    {
+        public string description;
+
+        public Product(string description)
+        {
+            this.description = description;
+        }
+
+        public void modificarDescripcion(string description)
+        {
+            this.description = description;
+        }
+    }
+}
